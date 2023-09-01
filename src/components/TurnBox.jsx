6bedef8515f1a60e8d29d59cac06ba719a3c61a8
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-09-01 17:55:44 +0900

    TurnBoxの修正
    Styled-Componentsに直接propsを渡さずにスタイルを変更できるように修正

--- src/components/TurnBox.jsx
+++ src/components/TurnBox.jsx
@@ -9,17 +9,14 @@ const StyledTurnBox = styled.div`
 const StyledTurn = styled.div`
   font-size: 1.5rem;
   padding: 10px 28px;
-  /* TODO : DOMにprops渡すとError出る */
-  ${(props) => props.isCircleTurn && 'border-bottom: 4px solid #000'};
+  ${(props) => props.$isCircleTurn && 'border-bottom: 4px solid #000'};
 `
 
 const TurnBox = ({ isCircleTurn }) => {
   return (
     <StyledTurnBox>
-      {/* <StyledTurn isCircleTurn={() => isCircleTurn}>⚪︎</StyledTurn>
-      <StyledTurn isCircleTurn={() => !isCircleTurn}>×</StyledTurn> */}
-      <StyledTurn>⚪︎</StyledTurn>
-      <StyledTurn>×</StyledTurn>
+      <StyledTurn $isCircleTurn={isCircleTurn}>⚪︎</StyledTurn>
+      <StyledTurn $isCircleTurn={!isCircleTurn}>×</StyledTurn>
     </StyledTurnBox>
   )
 }
